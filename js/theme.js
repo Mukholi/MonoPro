5b0b664a2ebb26c0f43acef197e9507e42a36d8c
BillMukholi <work.billmukholi@gmail.com>
2024-08-10 16:30:55 +0300

    Created A New Component - Colllage

--- js/theme.js
+++ js/theme.js
@@ -9,62 +9,6 @@ var mono_pro_component_collage_item_image = mono_pro_component_collage.querySele
 var mono_pro_component_collage_item_image_height = []
 var resizeIsActive = false;
 
-
-//GET ALL COLLAGE IMAGE HEIGHT
-
-// function alignCollageItem(){
-//     setTimeout(()=>{
-//         for (let counter = 0; counter < mono_pro_component_collage_item_image.length; counter++){
-//             mono_pro_component_collage_item_image_height.push(mono_pro_component_collage_item_image[counter].clientHeight)
-//         }
-//         if(window.innerWidth < breakPoint_767px){
-//             let internalCounterOffset = 2
-//             for (let counter = 0; counter < mono_pro_component_collage_item_image.length; counter++){
-//                 if (counter >= internalCounterOffset){
-//                     setTimeout(()=>{
-//                         let leftMarker = mono_pro_component_collage_item_image[1].getBoundingClientRect().left
-//                         let rightMarker = mono_pro_component_collage_item_image[0].getBoundingClientRect().right
-//                         let topMarker = mono_pro_component_collage_item_image[counter - internalCounterOffset].getBoundingClientRect().bottom
-//                         let bottomMarker = mono_pro_component_collage_item_image[counter].getBoundingClientRect().top
-//                         mono_pro_component_collage_item_container[counter].style.marginTop = "-"+((bottomMarker-topMarker)-(leftMarker-rightMarker))+"px"
-//                     },500)
-//                 }
-//             }
-//         }
-//         else{
-//             let internalCounterOffset = 3
-//             for (let counter = 0; counter < mono_pro_component_collage_item_image_height.length; counter++){
-//                 if (counter >= internalCounterOffset){
-//                     setTimeout(()=>{
-//                         let leftMarker = mono_pro_component_collage_item_image[1].getBoundingClientRect().left
-//                         let rightMarker = mono_pro_component_collage_item_image[0].getBoundingClientRect().right
-//                         let topMarker = mono_pro_component_collage_item_image[counter - internalCounterOffset].getBoundingClientRect().bottom
-//                         let bottomMarker = mono_pro_component_collage_item_image[counter].getBoundingClientRect().top
-//                         mono_pro_component_collage_item_container[counter].style.marginTop = "-"+(bottomMarker-topMarker)+"px"
-//                     },500)
-//                 }
-//             }
-    
-//         } 
-//         resizeIsActive = false 
-//         console.log("resized")
-//     },500)
-// }
-
-// alignCollageItem()
-// window.addEventListener('resize', ()=>{
-//     resizeIsActive = true;
-//     setTimeout(()=>{resizeIsActive = false},2000)
-//     resizeTimeout = setTimeout(alignCollageItem(),4000)
-//     for (let counter = 0; counter < mono_pro_component_collage_item_image.length; counter++){
-//         mono_pro_component_collage_item_container[counter].style.marginTop = "unset";
-//     }
-//     if(resizeIsActive){
-//         clearTimeout(resizeTimeout)
-//     }
-
-// })
-
 //CREATE MASONRY LAYOUT
 function createMasonryLayout(){
     let active_mono_pro_component_collage_item = []
@@ -115,7 +59,7 @@ window.addEventListener("resize",()=>{
 
 //HIDE AND UNHIDE COLLAGE ITEMS
 setTimeout(()=>{
-    if (mono_pro_component_selector_child){
+    if (mono_pro_component_selector_child == mono_pro_component_collage){
         for(let counter = breakPoint_0px; counter < mono_pro_component_selector_list_items.length; counter++){
             let itemText = mono_pro_component_selector_list_items[counter].querySelector("p").innerText
             mono_pro_component_selector_list_items[counter].addEventListener("click",()=>{
@@ -147,16 +91,6 @@ setTimeout(()=>{
                     for (let counter3 = 0; counter3 < selectedItems.length; counter3++){
                         let selectedItemChild = selectedItems[counter3].querySelector(".mono-pro-component-collage-item")
                         selectedItems[counter3].style.display = "none"
-                        // selectedItemChild.style.opacity = 0.5
-                        // gsap.to(selectedItemChild,{
-                        //     top:"101%",
-                        //     duration: "1"
-                        // })
-                        // setTimeout(() => {
-                        //     selectedItems[counter3].style.display = "none"
-
-                        // }, 1500);
- 
                     }
                     createMasonryLayout()
                 }
